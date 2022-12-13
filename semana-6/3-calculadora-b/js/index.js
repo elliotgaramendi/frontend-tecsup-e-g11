48dc9ed43c1a7b63be7305f470ad95c0a2d74b43
elliotgaramendi <elliotgaramendi@gmail.com>
2022-12-12 22:58:35 -0500

    s6.3 update js index calculateForm

--- semana-6/3-calculadora-b/js/index.js
+++ semana-6/3-calculadora-b/js/index.js
@@ -1,7 +1,36 @@
 'use strict';
 
 const documentReady = () => {
-  alert('Elliot');
+  const calculatorForm = document.getElementById('calculatorForm');
+
+  const calculate = (e) => {
+    e.preventDefault();
+    const number1 = +document.getElementById('number1').value;
+    const operator = document.getElementById('operator').value;
+    const number2 = +document.getElementById('number2').value;
+
+    let result = 0;
+
+    switch (operator) {
+      case '+':
+        result = number1 + number2;
+        break;
+      case '-':
+        result = number1 - number2;
+        break;
+      case '*':
+        result = number1 * number2;
+        break;
+      case '/':
+        result = number1 / number2;
+        break;
+      default:
+        break;
+    }
+    alert(result);
+  };
+
+  calculatorForm.addEventListener('submit', calculate);
 };
 
 document.addEventListener('DOMContentLoaded', documentReady);
