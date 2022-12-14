d3f1cb1d68312587c0c772406e7b38488684fddf
elliotgaramendi <elliotgaramendi@gmail.com>
2022-12-13 22:39:55 -0500

    s6.1 js index yesButton

--- semana-6/1-kierrez-zer-my-partner/js/index.js
+++ semana-6/1-kierrez-zer-my-partner/js/index.js
@@ -3,12 +3,20 @@
 const documentReady = () => {
   const yesButton = document.getElementById('yesButton');
   const noButton = document.getElementById('noButton');
-  
+
+  const getPartner = () => {
+    alert('💞 NoW ZoMoS PartNer 💞');
+    alert('♥ U Me DaZ LuZ & EsPerrAnZa A My Laif ♥');
+    alert('😱 OuR VoDa Is MaÑaNa 😱');
+    location.href = 'https://youtu.be/mos84UqOU3M?t=53';
+  };
+
   const brokenHeart = () => {
     noButton.style.top = Math.random() * innerHeight + 'px';
     noButton.style.left = Math.random() * innerWidth + 'px';
   };
 
+  yesButton.addEventListener('click', getPartner);
   noButton.addEventListener('mouseover', brokenHeart);
 };
 
