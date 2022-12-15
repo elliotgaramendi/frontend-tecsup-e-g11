0e6132610cbb5d3dc7eaf023093236bbf199bff7
elliotgaramendi <elliotgaramendi@gmail.com>
2022-12-14 22:05:39 -0500

    s7.1 update js index createPokemon

--- semana-7/1-pokemons-crud/js/index.js
+++ semana-7/1-pokemons-crud/js/index.js
@@ -4,25 +4,31 @@ const documentReady = () => {
   const tBodyPokemons = document.getElementById('tBodyPokemons');
   const formPokemon = document.getElementById('formPokemon');
 
-  tBodyPokemons.innerHTML += `
+  const createPokemon = (e) => {
+    e.preventDefault();
+    const documentFormPokemon = document.forms['formPokemon'];
+    const name = documentFormPokemon.name.value;
+    const type = documentFormPokemon.type.value;
+    const hp = documentFormPokemon.hp.value;
+    const attack = documentFormPokemon.attack.value;
+    const special = documentFormPokemon.special.value;
+    const imgUrl = documentFormPokemon.imgUrl.value;
+    // console.log({ name, type, hp, attack, special, imgUrl });
+    tBodyPokemons.innerHTML += `
     <tr>
       <td>1</td>
-      <td>Pikachu</td>
-      <td>Eléctrico</td>
-      <td>250</td>
-      <td>Trueno</td>
-      <td>Voltio</td>
+      <td>${name}</td>
+      <td>${type}</td>
+      <td>${hp}</td>
+      <td>${attack}</td>
+      <td>${special}</td>
       <td>
         <img
-          src="https://images.wikidexcdn.net/mwuploads/wikidex/thumb/7/77/latest/20150621181250/Pikachu.png/1200px-Pikachu.png"
-          alt="Pikachu" class="img-fluid" style="max-width: 128px;" />
+          src="${imgUrl}"
+          alt="${name}" class="img-fluid" style="max-width: 128px;" />
       </td>
     </tr>
   `;
-
-  const createPokemon = (e) => {
-    e.preventDefault();
-    console.log('Elliot');
   };
 
   formPokemon.addEventListener('submit', createPokemon);
