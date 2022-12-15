a4eea819fcbab05624fd4057dd2668c63b0af3b2
elliotgaramendi <elliotgaramendi@gmail.com>
2022-12-14 21:44:29 -0500

    s7.1 update js index formPokemon

--- semana-7/1-pokemons-crud/js/index.js
+++ semana-7/1-pokemons-crud/js/index.js
@@ -1,7 +1,31 @@
 'use strict';
 
 const documentReady = () => {
-  alert('Elliot Garamendi')
+  const tBodyPokemons = document.getElementById('tBodyPokemons');
+  const formPokemon = document.getElementById('formPokemon');
+
+  tBodyPokemons.innerHTML += `
+    <tr>
+      <td>1</td>
+      <td>Pikachu</td>
+      <td>Eléctrico</td>
+      <td>250</td>
+      <td>Trueno</td>
+      <td>Voltio</td>
+      <td>
+        <img
+          src="https://images.wikidexcdn.net/mwuploads/wikidex/thumb/7/77/latest/20150621181250/Pikachu.png/1200px-Pikachu.png"
+          alt="Pikachu" class="img-fluid" style="max-width: 128px;" />
+      </td>
+    </tr>
+  `;
+
+  const createPokemon = (e) => {
+    e.preventDefault();
+    console.log('Elliot');
+  };
+
+  formPokemon.addEventListener('submit', createPokemon);
 };
 
 document.addEventListener('DOMContentLoaded', documentReady);
