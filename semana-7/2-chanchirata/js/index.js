302c50bedb84da9c82dde8b11051ee3d6503ac0e
elliotgaramendi <elliotgaramendigmail.com>
2022-12-16 23:02:09 -0500

    s7.2 update js index module swiper

--- semana-7/2-chanchirata/js/index.js
+++ semana-7/2-chanchirata/js/index.js
@@ -1,23 +1,16 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js'
 
-export const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
+new Swiper('.swiper', {
+  autoplay: {
+    delay: 5000
+  },
   loop: true,
-
-  // If we need pagination
+  speed: 500,
   pagination: {
     el: '.swiper-pagination',
   },
-
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+  }
 });
